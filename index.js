99b2eb35f7034de5632f0a576fb833d62678754c
BerissaMuyizere <muyizereberissa@gmail.com>
2025-06-05 21:53:09 +0300

    added on click event listen on a button

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const background = document.getElementById('body')
 
 background.style.backgroundColor = "silver";
-background.style.alignContent = 'center'
+background.style.alignContent = 'center';
+background.style.marginLeft = '10%'
 
 const title = document.getElementById("title");
 
@@ -14,6 +15,7 @@ headings.forEach(heading => {
 
 const fruitsList = document.getElementById("fruits");
 fruitsList.style.display = 'flex';
+fruitsList.style.backgroundColor = '#b97d65';
 
 const imageStyles = document.getElementById('image');
 imageStyles.style.width = '280px';
@@ -55,14 +57,16 @@ fruitsList.appendChild(newFruit);
 
 const vegesList = document.getElementById("veges");
 vegesList.style.display = 'flex';
+vegesList.style.backgroundColor = '#b97d65'
 
 const newVegetable = document.createElement('img')
 newVegetable.src = './images/onions.jpg';
 newVegetable.style.width = '280px';
 newVegetable.style.height = '280px';
-newVegetable.style.marginTop = '23px'
+newVegetable.style.marginTop = '23px';
+newVegetable.textContent = 'Carrots'
 
-const newVeg = document.createElement("li");
+const newVeg = document.createElement("figcaption");
 newVeg.textContent = "Carrots";
 vegesList.appendChild(newVeg);
 vegesList.appendChild(newVegetable)
@@ -72,5 +76,9 @@ addFruitButton.onclick = ()=>{
   fruitsList.style.backgroundColor = '#5ACD32'
 }
 
+const addVegButton = document.getElementById('addVegeButton');
+addVegButton.onclick = ()=>{
+  vegesList.style.backgroundColor = '#5ACD32'
+}
 
 
